b5f644f7a38c7b424d1b20eb7e152973238ab0eb
Luis Enrique <luizhuaman94@gmail.com>
2025-08-24 14:28:33 -0500

    Se agregan notas de los attr y metodos estaticos

--- src/platzi/play/util/ScannerUtils.java
+++ src/platzi/play/util/ScannerUtils.java
@@ -3,8 +3,12 @@ package platzi.play.util;
 import java.util.Scanner;
 
 public class ScannerUtils {
+    //Un atributo estático pertenece directamente a la clase y no a un objeto individual,
+    //por lo que su valor es compartido entre todas las instancias.
+    //muy utiles para hacer conteos, validaciones o clases utilitarias
     public static Scanner scanner = new Scanner(System.in);
 
+    //metodos estaticos para que se puedan mostrar en el main
     public static String capturarTexto(String mensaje) {
         System.out.println(mensaje + ": ");
         return scanner.nextLine();
@@ -14,7 +18,7 @@ public class ScannerUtils {
         System.out.println(mensaje + ": ");
 
         int dato = scanner.nextInt();
-        scanner.nextLine();
+        scanner.nextLine(); //cuando el usuario da enter lo obviamos con esta linea
         return dato;
     }
 
